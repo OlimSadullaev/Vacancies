a1176bbe3a8c2e48c8f7808456865dfc3c6e232b
Olim Sadullaev <olimxonnutfullaevich@gmail.com>
2025-01-06 14:49:40 +0500

    DTO duplicate changed.

--- Vacancies/DTOs/GrantDTO.cs
+++ Vacancies/DTOs/GrantDTO.cs
@@ -1,25 +1,23 @@
 ﻿namespace Vacancies.DTOs
 {
-    public class GrantDTO
+    public class BaseGrantDTO
     {
-        public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public string Country { get; set; }
         public DateTime Deadline { get; set; }
         public string Requirements { get; set; }
         public string FundingAmount { get; set; }
+    }
+
+    public class GrantDTO : BaseGrantDTO
+    {
+        public int Id { get; set; }
         public List<CategoryDTO> Categories { get; set; }
     }
 
-    public class CreateGrantDTO
+    public class CreateGrantDTO : BaseGrantDTO
     {
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public string Country { get; set; }
-        public DateTime Deadline { get; set; }
-        public string Requirements { get; set; }
-        public string FundingAmount { get; set; }
         public List<Guid> CategoryIds { get; set; }
     }
 }
